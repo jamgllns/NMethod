5168e82cb9caaec29309bc646d27756b5e8d38ec
jamgllns <157282679+jamgllns@users.noreply.github.com>
2024-06-16 15:32:43 +0800

    Add files via upload

---
+++ NMethod-20240616T033554Z-001/NMethod/src/nmethod/NMethod.java
@@ -0,0 +1,9 @@
+package nmethod;
+public class NMethod {
+    public static void main(String[] args) {
+        main frame = new main();
+        frame.setVisible(true);
+        frame.setLocationRelativeTo(null);
+    }
+    
+}
